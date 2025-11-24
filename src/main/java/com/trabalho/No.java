83c62baa9594cf6d2de06b8da4d7612e187d502a
ArthurFritz8 <geminepestudante5@gmail.com>
2025-11-23 21:35:05 -0300

    No

---
+++ src/main/java/com/trabalho/No.java
@@ -0,0 +1,15 @@
+package com.trabalho;
+
+public class No {
+    int valor;
+    No esquerda;
+    No direita;
+    int altura;
+
+    public No(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+        this.altura = 1;
+    }
+}
