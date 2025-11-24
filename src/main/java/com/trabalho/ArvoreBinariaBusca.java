c69eec55413a9ece35f6faf569260c3325ec44c9
ArthurFritz8 <geminepestudante5@gmail.com>
2025-11-23 22:48:12 -0300

    ArvoreBinariaBusca

---
+++ src/main/java/com/trabalho/ArvoreBinariaBusca.java
@@ -0,0 +1,40 @@
+package com.trabalho;
+
+public class ArvoreBinariaBusca {
+    No raiz;
+
+    public ArvoreBinariaBusca() {
+        raiz = null;
+    }
+
+    public void inserir(int valor) {
+        raiz = inserirRecursivo(raiz, valor);
+    }
+
+    private No inserirRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return new No(valor);
+        }
+
+        if (valor < atual.valor) {
+            atual.esquerda = inserirRecursivo(atual.esquerda, valor);
+        } else if (valor > atual.valor) {
+            atual.direita = inserirRecursivo(atual.direita, valor);
+        }
+        return atual;
+    }
+
+    public boolean buscar(int valor) {
+        return buscarRecursivo(raiz, valor);
+    }
+
+    private boolean buscarRecursivo(No atual, int valor) {
+        if (atual == null) {
+            return false;
+        }
+        if (valor == atual.valor) {
+            return true;
+        }
+        return valor < atual.valor ? buscarRecursivo(atual.esquerda, valor) : buscarRecursivo(atual.direita, valor);
+    }
+}
